83fbe621e5d77c488d134d53c123ad0494b68cd5
lauramainero <mainerolaura@gmail.com>
2025-03-29 21:01:11 -0300

    add: parameter on planejarSemestre

--- planejaMatricula.h
+++ planejaMatricula.h
@@ -20,7 +20,7 @@ bool verificaMax3PorDia(Aluno *aluno);
 bool disciplinaJaCursada(Aluno *aluno, Disciplina disciplina);
 
 // Planeja o semestre do discente.
-void planejarSemestre(Aluno *aluno, Disciplina obrigatorias[], int num_obrigatorias);
+void planejarSemestre(Aluno *aluno, Disciplina obrigatorias[], int numObrigatorias, int numEletivas);
 
 // Verifica para quais dias da semana o discente possui aulas planejadas.
 void extrairDiasSemana(char horario[], bool dias[5]);
